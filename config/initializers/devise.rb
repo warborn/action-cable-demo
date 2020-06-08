a983766be6b9237a1682d45bf931948a237e0331
Iván Munguia <ivanalejandro249@gmail.com>
2020-06-08 15:39:15 -0500

    Add support for user creation and login/logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,7 +301,7 @@ Devise.setup do |config|
     jwt.secret = Rails.application.credentials.jwt_secret
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
-    ],
+    ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
