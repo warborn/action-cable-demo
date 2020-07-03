077cb7244fb86af9c60d28b27ac9f50f613044a2
Iván Munguia <ivanalejandro249@gmail.com>
2020-07-03 17:36:42 -0500

    Add trackable devise strategy to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable,
+         :recoverable, :validatable, :trackable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :notifications
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module ActionCableDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'Mexico City'
+    config.active_record.default_timezone = :local
   end
 end
