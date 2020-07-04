ae5dbac259389ea96cee61033598167961a82506
Iván Munguia <ivanalejandro249@gmail.com>
2020-07-03 19:37:54 -0500

    Add background job to count missed notifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    super { |resource| LastLoginChangesJob.set(wait: 3.seconds).perform_later(resource) }
+  end
+
   private
 
   def respond_with(resource, _opts = {})
---
+++ app/jobs/last_login_changes_job.rb
@@ -0,0 +1,56 @@
+class Summary
+  attr_reader :summary
+
+  def initialize(notifications)
+    @summary = get_summary_from_notifications(notifications)
+  end
+
+  def to_h
+    summary.to_h
+  end
+
+  def to_human_readable
+    summary.map do |k, v|
+      pluralized = v > 1 ? k.pluralize : k
+      [k, "#{v} #{pluralized.capitalize}"]
+    end.to_h
+  end
+
+  def get_summary_from_notifications(notifications)
+    summary = {}
+    notifications.each do |notification|
+      key = notification.category
+      summary[key] = (summary[key] || 0) + 1
+    end
+    summary
+  end
+end
+
+
+class LastLoginChangesJob < ApplicationJob
+  queue_as :default
+
+  def perform(user)
+    notifications = get_external_notifications(user)
+    summary = get_summary_of_notifications(notifications)
+
+    puts "****PERFORMING JOB: ", summary.inspect
+
+    return if summary.empty?
+
+    ActionCable.server.broadcast 'notifications_channel', {
+      type: "summary",
+      message: "Notifications received while you were gone",
+      content: summary
+    }
+  end
+
+  def get_external_notifications(user)
+    Notification.where.not(user_id: user.id).where('created_at > ?', user.last_sign_in_at)
+  end
+
+  def get_summary_of_notifications(input)
+    notifications = input.to_a
+    Summary.new(input).to_human_readable
+  end
+end
