928fa541869d09ab6ecd20e838e32cdc5962c89d
Iván Munguia <ivanalejandro249@gmail.com>
2020-07-06 12:54:53 -0500

    Fix devise routes default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: []
   devise_scope :user do
-    post '/signup', to: 'registrations#create'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
+    post '/signup', to: 'registrations#create', defaults: {format: :json}
+    post '/login', to: 'sessions#create', defaults: {format: :json}
+    delete '/logout', to: 'sessions#destroy', defaults: {format: :json}
   end
 
   get '/users/me', to: 'users#me'
